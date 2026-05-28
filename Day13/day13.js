53b7c1819fce51a9102bf412ac4951be498bfc19
kalyaaan24 <kalyandhamala24@gmail.com>
2026-05-28 13:19:45 +0545

    Learning Utsav all days learning

---
+++ Day13/day13.js
@@ -0,0 +1,32 @@
+
+
+// console.log("Hello World")
+// console.log("Starting to learn Javascript")
+
+
+
+
+
+
+
+
+
+// var a=7
+// console.log(a)
+// a="Kalyan"
+// console.log(a)
+
+
+
+
+
+let a=7;
+let b=null;
+let c="Kalyan"
+let d= Symbol("Symbol no 1")
+let e= BigInt(19999)
+let f= true;
+let g= undefined
+console.log(a,b,c,d,e,f,g)
+console.log(typeof a)
+console.log (typeof f)
